537538ad1b97d342825ab28728aeac357c451dbe
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-07 22:48:27 +0330

    i wrote productWbUnit Tests

---
+++ test/productWbUnitTest.test.js
@@ -0,0 +1,147 @@
+var expect = require('chai').expect;
+const sinon = require('sinon');
+const { ProductWbRouterClass } = require('../dist/app/productWeb/productWbRouterClass');
+const { ProductWbBusConc } = require('../dist/app/productWeb/productWbBus');
+const { ProductWbDalConc } = require('../dist/app/productWeb/productWbDal');
+const { ProductSchema } = require('../dist/app/product/productEntity');
+const { ResponseStatus } = require('../dist/app/utility/errorStatus');
+describe("productWbRouterClass Unit Tests", function () {
+     describe("findAll", function () {
+          it("1- findAll should response 200", async function () {
+               const mRes = {
+                    status: sinon.stub().returnsThis(),
+                    send: sinon.stub(),
+               };
+
+               const mReq = {};
+
+               const bus = new ProductWbBusConc(new ProductWbDalConc());
+               sinon.stub(bus, "findAll").returns([]);
+
+               const tester = new ProductWbRouterClass(bus);
+               //sinon.stub(LikeEntitySchema, "validate").returns({ error: true });
+               const response = await tester.findAll(mReq, mRes, (e) => { console.log(e) });
+               expect(response).to.have.property('status').equal(200);
+               bus.findAll.restore(); //don't forget to restore
+               //LikeEntitySchema.validate.restore(); //don't forget to restore
+          });
+
+
+
+          it("2- findOneProduct should response 400 if webuserId is undefiend", async function () {
+
+               const mRes = {
+                    status: sinon.stub().returnsThis(),
+                    send: sinon.stub(),
+               };
+
+               const mReq = {
+                    query: {
+                         //webuserId: '64b6c8f0b10'
+                    },
+                    params: {  }
+               };
+
+               const bus = new ProductWbBusConc(new ProductWbDalConc());
+               sinon.stub(bus, "findOne").returns({
+                    "acknowledged": true,
+                    "insertedId": "6571769ef9e06365aab1c7c8"
+               });
+
+               const tester = new ProductWbRouterClass(bus);
+               //sinon.stub(ProductSchema, "validate").returns({ error: true });
+               const response = await tester.findOne(mReq, mRes, (e) => { console.log(e) });
+               expect(response).to.have.property('status').equal(400);
+               bus.findOne.restore(); //don't forget to restore
+               //ProductSchema.validate.restore(); //don't forget to restore
+          });
+
+
+          it("3- findOneProduct should response 400 if is undefiend", async function () {
+
+               const mRes = {
+                    status: sinon.stub().returnsThis(),
+                    send: sinon.stub(),
+               };
+
+               const mReq = {
+                    query: {
+                         webuserId: '64b6c8f0b10'
+                    },
+                    params: {  }
+               };
+
+               const bus = new ProductWbBusConc(new ProductWbDalConc());
+               sinon.stub(bus, "findOne").returns({
+                    "acknowledged": true,
+                    "insertedId": "6571769ef9e06365aab1c7c8"
+               });
+
+               const tester = new ProductWbRouterClass(bus);
+               //sinon.stub(ProductSchema, "validate").returns({ error: true });
+               const response = await tester.findOne(mReq, mRes, (e) => { console.log(e) });
+               expect(response).to.have.property('status').equal(400);
+               bus.findOne.restore(); //don't forget to restore
+               //ProductSchema.validate.restore(); //don't forget to restore
+          });
+
+
+          it("4- findOneProduct should response 400 if is entity is invalid", async function () {
+
+               const mRes = {
+                    status: sinon.stub().returnsThis(),
+                    send: sinon.stub(),
+               };
+
+               const mReq = {
+                    query: {
+                         webuserId: '64b6c8f0b10'
+                    },
+                    params: { id:'sss' }
+               };
+
+               const bus = new ProductWbBusConc(new ProductWbDalConc());
+               sinon.stub(bus, "findOne").returns({
+                    "acknowledged": true,
+                    "insertedId": "6571769ef9e06365aab1c7c8"
+               });
+
+               const tester = new ProductWbRouterClass(bus);
+               sinon.stub(ProductSchema, "validate").returns({ error: true });
+               const response = await tester.findOne(mReq, mRes, (e) => { console.log(e) });
+               expect(response).to.have.property('status').equal(400);
+               bus.findOne.restore(); //don't forget to restore
+               ProductSchema.validate.restore(); //don't forget to restore
+          });
+
+
+
+          it("4- findOneProduct should response 200 if is everything is ok", async function () {
+
+               const mRes = {
+                    status: sinon.stub().returnsThis(),
+                    send: sinon.stub(),
+               };
+
+               const mReq = {
+                    query: {
+                         webuserId: '64b6c8f0b10'
+                    },
+                    params: { id:'6571769ef9e06365aab1c7c8' }
+               };
+
+               const bus = new ProductWbBusConc(new ProductWbDalConc());
+               sinon.stub(bus, "findOne").returns({
+                    "acknowledged": true,
+                    "insertedId": "6571769ef9e06365aab1c7c8"
+               });
+
+               const tester = new ProductWbRouterClass(bus);
+               sinon.stub(ProductSchema, "validate").returns({ error: false });
+               const response = await tester.findOne(mReq, mRes, (e) => { console.log(e) });
+               expect(response).to.have.property('status').equal(200);
+               bus.findOne.restore(); //don't forget to restore
+               ProductSchema.validate.restore(); //don't forget to restore
+          });
+     });
+});
