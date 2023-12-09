7f53f00937434cf878a6d2b457d8b627030ebbfb
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-09 23:18:43 +0330

    i added faqLogger

---
+++ app/logger/faqLogger.ts
@@ -0,0 +1,113 @@
+import { IBaseLogger } from "./iBaseLogger";
+import { WinstonLogger } from "./winstonLogger";
+
+
+export class FaqDalLogger implements IBaseLogger {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("FaqDal");
+  }
+  logError(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+  logInfo(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+
+
+export class FaqRouterLogger implements IBaseLogger {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("FaqRouter");
+  }
+
+  logError(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+  logInfo(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+
+
+export class FaqRouterClassLogger implements IBaseLogger  {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("FaqRouterClass");
+  }
+
+  logError(err: string,method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+
+  logInfo(err: string,method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+
+
+
+export class FaqWbDalLogger implements IBaseLogger {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("FaqWbDal");
+  }
+  logError(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+  logInfo(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+
+
+export class FaqWbRouterLogger implements IBaseLogger {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("FaqWbRouter");
+  }
+
+  logError(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+  logInfo(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+
+
+export class FaqWbRouterClassLogger implements IBaseLogger  {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("FaqWbRouterClass");
+  }
+
+  logError(err: string,method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+
+  logInfo(err: string,method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+module.exports = {
+  FaqRouterClassLogger,
+  FaqRouterLogger,
+  FaqDalLogger,
+  FaqWbDalLogger,
+  FaqWbRouterLogger,
+  FaqWbRouterClassLogger
+}
