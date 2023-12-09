8e5ed673c93d71cc49f400fa96e69cd265755302
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-09 23:19:16 +0330

    i added faqWeb component

---
+++ app/faqWeb/faqWbBus.ts
@@ -0,0 +1,21 @@
+import { FaqEntity } from "../faq/faqEntity";
+import { FaqWbDal } from "./faqWbDal";
+
+export interface FaqWbBus {
+  findAll(): Promise<FaqEntity[]>;
+}
+
+export class FaqWbBusConc implements FaqWbBus {
+  private db: FaqWbDal;
+  constructor(db: FaqWbDal) {
+    this.db = db;
+  }
+  async findAll(): Promise<FaqEntity[]> {
+    const result = await this.db.findAll();
+    return result;
+  }
+}
+
+module.exports = {
+  FaqWbBusConc,
+};
---
+++ app/faqWeb/faqWbDal.ts
@@ -0,0 +1,29 @@
+import { MongoDb } from "../config/mongodb";
+import { FaqEntity } from "../faq/faqEntity";
+import { FaqWbDalLogger } from "../logger/faqLogger";
+
+export interface FaqWbDal {
+  findAll(): Promise<FaqEntity[]>;
+}
+
+export class FaqWbDalConc implements FaqWbDal {
+  logger: any;
+  constructor() {
+    this.logger = new FaqWbDalLogger();
+  }
+  async findAll(): Promise<FaqEntity[]> {
+    let result;
+    try {
+      const collection = MongoDb.dbconnect("faqs");
+      await collection.then((faqs) => {
+        result = faqs
+          .find({ display: true })
+          .sort({ priority: 1 })
+          .toArray();
+      });
+    } catch (err: any) {
+      this.logger.logError(err, "findAll");
+    }
+    return result;
+  }
+}
