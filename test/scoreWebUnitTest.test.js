bdc1aa8919a61a9e181f6c6e35363994eb1716f4
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-11 12:03:26 +0330

    i edited schema's name

--- test/scoreWebUnitTest.test.js
+++ test/scoreWebUnitTest.test.js
@@ -3,9 +3,9 @@ const sinon = require('sinon');
 const { ScoreWbRouterClass } = require('../dist/app/scoreWeb/scoreWbRouterClass');
 const { ScoreWbBusConc } = require('../dist/app/scoreWeb/scoreWbBus');
 const { ScoreWbDalConc } = require('../dist/app/scoreWeb/scoreWbDal');
-const { ScoreEntitySchema } = require('../dist/app/score/scoreEntity');
+const { ScoreSchema } = require('../dist/app/score/scoreEntity');
 const { ResponseStatus } = require('../dist/app/utility/errorStatus');
-
+//to run ==> npm test -- -g "ScoreWbRouterClass"
 describe("ScoreWbRouterClass", function () {
      describe("updateOneScore", function () { 
           it("1- updateOneScore should response 400 if id is undefiend", async function () {
@@ -27,11 +27,13 @@ describe("ScoreWbRouterClass", function () {
                });
 
                const tester = new ScoreWbRouterClass(bus);
-               //sinon.stub(LikeEntitySchema, "validate").returns({ error: true });
+               //sinon.stub(ScoreSchema, "validate").returns({ error: true });
                const response = await tester.updateOneScore(mReq, mRes, (e) => { console.log(e) });
                expect(response).to.have.property('status').equal(400);
+               expect(response).to.have.property('message').equal('validation failed. id is not provided');
+
                bus.updateOneScore.restore(); //don't forget to restore
-               //LikeEntitySchema.validate.restore(); //don't forget to restore
+               //ScoreSchema.validate.restore(); //don't forget to restore
           });
           
           it("2- updateOneScore should response 400 if id is invalid", async function () {
@@ -53,11 +55,14 @@ describe("ScoreWbRouterClass", function () {
                });
 
                const tester = new ScoreWbRouterClass(bus);
-               //sinon.stub(LikeEntitySchema, "validate").returns({ error: true });
+               //sinon.stub(ScoreSchema, "validate").returns({ error: true });
                const response = await tester.updateOneScore(mReq, mRes, (e) => { console.log(e) });
+               console.log(response);
                expect(response).to.have.property('status').equal(400);
+              // expect(response).to.have.property('message').equal('validation failed. id is not valid');
+
                bus.updateOneScore.restore(); //don't forget to restore
-               //LikeEntitySchema.validate.restore(); //don't forget to restore
+               //ScoreSchema.validate.restore(); //don't forget to restore
           });
 
           it("3- updateOneScore should response 400 if scoreEntity is invalid", async function () {
@@ -79,11 +84,11 @@ describe("ScoreWbRouterClass", function () {
                });
 
                const tester = new ScoreWbRouterClass(bus);
-               sinon.stub(ScoreEntitySchema, "validate").returns({ error: true });
+               sinon.stub(ScoreSchema, "validate").returns({ error: true });
                const response = await tester.updateOneScore(mReq, mRes, (e) => { console.log(e) });
                expect(response).to.have.property('status').equal(400);
                bus.updateOneScore.restore(); //don't forget to restore
-               ScoreEntitySchema.validate.restore(); //don't forget to restore
+               ScoreSchema.validate.restore(); //don't forget to restore
           });
 
           it("4- updateOneScore should response 200 if everything is ok", async function () {
@@ -105,11 +110,11 @@ describe("ScoreWbRouterClass", function () {
                });
 
                const tester = new ScoreWbRouterClass(bus);
-               sinon.stub(ScoreEntitySchema, "validate").returns({ error: false });
+               sinon.stub(ScoreSchema, "validate").returns({ error: false });
                const response = await tester.updateOneScore(mReq, mRes, (e) => { console.log(e) });
                expect(response).to.have.property('status').equal(200);
                bus.updateOneScore.restore(); //don't forget to restore
-               ScoreEntitySchema.validate.restore(); //don't forget to restore
+               ScoreSchema.validate.restore(); //don't forget to restore
           });
      });
 });
