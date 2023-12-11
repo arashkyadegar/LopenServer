35866fe8a6237ca0ca622a4402a6dff5c91a5322
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-11 12:07:55 +0330

    i added expect for validation text

--- test/productWbUnitTest.test.js
+++ test/productWbUnitTest.test.js
@@ -5,6 +5,7 @@ const { ProductWbBusConc } = require('../dist/app/productWeb/productWbBus');
 const { ProductWbDalConc } = require('../dist/app/productWeb/productWbDal');
 const { ProductSchema } = require('../dist/app/product/productEntity');
 const { ResponseStatus } = require('../dist/app/utility/errorStatus');
+//to run ==> npm test -- -g "productWbRouterClass"
 describe("productWbRouterClass", function () {
      describe("findOne", async function(){
           it("1- findOne should response 400 if webuserId is undefiend", async function () {
@@ -16,7 +17,7 @@ describe("productWbRouterClass", function () {
 
                const mReq = {
                     query: {
-                         //webuserId: '64b6c8f0b10'
+                         //wbuserId: '64b6c8f0b10'
                     },
                     params: {  }
                };
@@ -31,12 +32,14 @@ describe("productWbRouterClass", function () {
                //sinon.stub(ProductSchema, "validate").returns({ error: true });
                const response = await tester.findOne(mReq, mRes, (e) => { console.log(e) });
                expect(response).to.have.property('status').equal(400);
+               expect(response).to.have.property('message').equal(`validation failed. wbuserId is not provided`);
+
                bus.findOne.restore(); //don't forget to restore
                //ProductSchema.validate.restore(); //don't forget to restore
           });
 
 
-          it("2- findOne should response 400 if is undefiend", async function () {
+          it("2- findOne should response 400 if id is undefined", async function () {
 
                const mRes = {
                     status: sinon.stub().returnsThis(),
@@ -45,7 +48,7 @@ describe("productWbRouterClass", function () {
 
                const mReq = {
                     query: {
-                         webuserId: '64b6c8f0b10'
+                         wbuserId: '6571769ef9e06365aab1c7c8'
                     },
                     params: {  }
                };
@@ -60,6 +63,7 @@ describe("productWbRouterClass", function () {
                //sinon.stub(ProductSchema, "validate").returns({ error: true });
                const response = await tester.findOne(mReq, mRes, (e) => { console.log(e) });
                expect(response).to.have.property('status').equal(400);
+               expect(response).to.have.property('message').equal('validation failed. id is not provided');
                bus.findOne.restore(); //don't forget to restore
                //ProductSchema.validate.restore(); //don't forget to restore
           });
@@ -74,7 +78,7 @@ describe("productWbRouterClass", function () {
 
                const mReq = {
                     query: {
-                         webuserId: '64b6c8f0b10'
+                         wbuserId: '6571769ef9e06365aab1c7c8'
                     },
                     params: { id:'sss' }
                };
@@ -104,7 +108,7 @@ describe("productWbRouterClass", function () {
 
                const mReq = {
                     query: {
-                         webuserId: '64b6c8f0b10'
+                         wbuserId: '6571769ef9e06365aab1c7c8'
                     },
                     params: { id:'6571769ef9e06365aab1c7c8' }
                };
