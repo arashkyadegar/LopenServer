71405d92d3484df987ad2d9d78dbb80943aecde5
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-12 22:49:53 +0330

    i change some variable names

--- app/likeWeb/likeWb.ts
+++ app/likeWeb/likeWb.ts
@@ -1,39 +1,35 @@
-import express from 'express';
-import { LikeWbBusConc } from './likeWbBus';
-import { LikeWbDalConc } from './likeWbDal';
-import { LikeWbRouterClass } from './likeWbRouterClass';
-import { LikeWbRouterLogger } from '../logger/likeWbLogger';
+import express from "express";
+import { LikeWbBusConc } from "./likeWbBus";
+import { LikeWbDalConc } from "./likeWbDal";
+import { LikeWbRouterClass } from "./likeWbRouterClass";
+import { LikeWbRouterLogger } from "../logger/likeWbLogger";
 
 export const LikeWbRouter = express.Router();
 
-LikeWbRouter.post("/",async function(req,res,next){
-  try{
-      const LikeWbBus = new LikeWbBusConc(new LikeWbDalConc());
-      const router = new LikeWbRouterClass(LikeWbBus);
-      const result = await router.createOneLike(req,res,next);
-      return res.status(result.status).send(result.message);
-    }catch(err: any) {
-        const logger = new LikeWbRouterLogger();
-        logger.logError(err,"post/");
-        next(err);
-    }
- });
-
-LikeWbRouter.delete("/",async function(req,res,next){
-  try{
-    const LikeWbBus = new LikeWbBusConc(new LikeWbDalConc());
-    const router = new LikeWbRouterClass(LikeWbBus);
-    const result = await router.deleteOneLike(req,res,next);
+LikeWbRouter.post("/", async function (req, res, next) {
+  try {
+    const bus = new LikeWbBusConc(new LikeWbDalConc());
+    const router = new LikeWbRouterClass(bus);
+    const result = await router.createOneLike(req, res, next);
     return res.status(result.status).send(result.message);
-  }catch(err: any) {
-      const logger = new LikeWbRouterLogger();
-      logger.logError(err,"delete/");
-      next(err);
+  } catch (err: any) {
+    const logger = new LikeWbRouterLogger();
+    logger.logError(err, "post/");
+    next(err);
   }
-
 });
 
+LikeWbRouter.delete("/", async function (req, res, next) {
+  try {
+    const bus = new LikeWbBusConc(new LikeWbDalConc());
+    const router = new LikeWbRouterClass(bus);
+    const result = await router.deleteOneLike(req, res, next);
+    return res.status(result.status).send(result.message);
+  } catch (err: any) {
+    const logger = new LikeWbRouterLogger();
+    logger.logError(err, "delete/");
+    next(err);
+  }
+});
 
 module.exports = LikeWbRouter;
-
-
