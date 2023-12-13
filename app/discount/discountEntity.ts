33ca3cbb8ce798104f5ef77225b2159704dad081
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-13 10:36:01 +0330

    i added discountEntity

---
+++ app/discount/discountEntity.ts
@@ -0,0 +1,33 @@
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+
+export const discountTypes = {
+  GENERAL: 1, // for all products
+  SPECIFIED: 2, // for specified product
+};
+
+export class DiscountEntity {
+  _id: string = "";
+  sDate!: Date;
+  eDate!: Date;
+  type: number = 0;
+  value: number = 0;
+  productId: string = "";
+  date!: Date;
+}
+
+export const DiscountSchema = Joi.object({
+  _id: Joi.objectId().allow(""),
+  sDate: Joi.string(),
+  eDate: Joi.string(),
+  type: Joi.number(),
+  value: Joi.number(),
+  productId: Joi.string().allow(""),
+  date: Joi.string().allow(""),
+});
+
+module.exports = {
+  DiscountEntity,
+  DiscountSchema,
+  discountTypes,
+};
