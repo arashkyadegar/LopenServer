1216b2178c824643cd7c509ab7686cf7f0683c98
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-13 11:08:25 +0330

    i created discountLogger

---
+++ app/logger/discountLogger.ts
@@ -0,0 +1,113 @@
+import { IBaseLogger } from "./iBaseLogger";
+import { WinstonLogger } from "./winstonLogger";
+
+
+export class DiscountDalLogger implements IBaseLogger {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("DiscountDal");
+  }
+  logError(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+  logInfo(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+
+
+export class DiscountRouterLogger implements IBaseLogger {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("DiscountRouter");
+  }
+
+  logError(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+  logInfo(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+
+
+export class DiscountRouterClassLogger implements IBaseLogger  {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("DiscountRouterClass");
+  }
+
+  logError(err: string,method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+
+  logInfo(err: string,method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+
+
+
+export class DiscountWbDalLogger implements IBaseLogger {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("DiscountWbDal");
+  }
+  logError(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+  logInfo(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+
+
+export class DiscountWbRouterLogger implements IBaseLogger {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("DiscountWbRouter");
+  }
+
+  logError(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+  logInfo(err: string, method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+
+
+export class DiscountWbRouterClassLogger implements IBaseLogger  {
+  logger: WinstonLogger;
+  constructor(){
+    this.logger= new WinstonLogger("DiscountWbRouterClass");
+  }
+
+  logError(err: string,method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.error(err);
+  }
+
+  logInfo(err: string,method: string): void {
+    const instance = this.logger.getLogger(method);
+    instance.info(err);
+  }
+}
+module.exports = {
+  DiscountRouterClassLogger,
+  DiscountRouterLogger,
+  DiscountDalLogger,
+  DiscountWbDalLogger,
+  DiscountWbRouterLogger,
+  DiscountWbRouterClassLogger
+}
