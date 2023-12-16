6261d3fd4ad2a5052b0e7bf3f74b94aa4a74e7f5
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-17 01:20:38 +0330

    i fixed files ref

--- app/productWeb/productWbBus.ts
+++ app/productWeb/productWbBus.ts
@@ -2,7 +2,9 @@ import { date } from "joi";
 import { ProductEntity, ProductWbEntity } from "../product/productEntity";
 import { ProductWbDal } from "./productWbDal";
 import { DiscountEntity } from "../discount/discountEntity";
-import { checkLikesByUser } from "../likeWeb/likeWebUtility";
+import { checkLikesByUser } from "../likeWeb/likeWbUtility";
+import { calculateScore } from "../scoreWeb/scoreWbUtility";
+import { checkIfDiscountIsAllowed } from "../discount/discountUtility";
 var _ = require("lodash");
 
 export interface ProductWbBus {
@@ -20,10 +22,10 @@ export class ProductWbBusConc implements ProductWbBus {
     const today = new Date();
     const result = await this.db.findAll(today.toISOString());
     result.forEach((element) => {
-      element.score = this.calculateScore(element);
+      element.score = calculateScore(element);
       if (element.discounts.length > 0) {
         let discount = _.first(element.discounts);
-        let temp = this.checkIfDiscountIsAllowed(discount);
+        let temp = checkIfDiscountIsAllowed(discount);
         element.discounts = temp;
       }
     });
@@ -36,7 +38,7 @@ export class ProductWbBusConc implements ProductWbBus {
     //check if product discount is allowed or not
     if (result[0].discounts.length > 0) {
       let element = _.first(result[0].discounts);
-      let temp = this.checkIfDiscountIsAllowed(element);
+      let temp = checkIfDiscountIsAllowed(element);
       result[0].discounts = temp;
     }
     //check if product discount is allowed or not/////end
@@ -45,7 +47,7 @@ export class ProductWbBusConc implements ProductWbBus {
     const scoreCount = result[0].scores.length;
     let totalScore = 0;
     result[0].score = totalScore / scoreCount;
-    result[0].score = this.calculateScore(result[0]);
+    result[0].score = calculateScore(result[0]);
     //calculating product's score ///////////////////////end
 
     //check if user has liked the product or not
@@ -55,30 +57,6 @@ export class ProductWbBusConc implements ProductWbBus {
     //check if user has liked the product or not////////////end
     return result;
   }
-  calculateScore(entity: ProductWbEntity): number {
-    const scoreCount = entity.scores.length;
-    let totalScore = 0;
 
-    entity.scores.forEach((element) => {
-      totalScore = totalScore + element.value;
-    });
 
-    return totalScore / scoreCount;
-  }
-  checkIfDiscountIsAllowed(discount: any): DiscountEntity[] {
-    let today = new Date();
-    const sd = discount.sDate.toISOString().substring(0, 10);
-    const ed = discount.eDate.toISOString().substring(0, 10);
-    const td = today.toISOString().substring(0, 10);
-
-    console.log(td);
-    console.log(sd);
-    console.log(ed);
-
-    if (td >= sd && td <= ed) {
-      return discount;
-    } else {
-      return [];
-    }
-  }
 }
