6a6300aeba66cae6fa57d36dfa93530279d76ffb
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-17 01:17:40 +0330

    i moved checkIfDiscountIsAllowed to discountUtility..ts

---
+++ app/discount/discountUtility.ts
@@ -0,0 +1,18 @@
+import { DiscountEntity } from "./discountEntity";
+
+export const checkIfDiscountIsAllowed = (discount: any): DiscountEntity[] {
+     let today = new Date();
+     const sd = discount.sDate.toISOString().substring(0, 10);
+     const ed = discount.eDate.toISOString().substring(0, 10);
+     const td = today.toISOString().substring(0, 10);
+ 
+     // console.log(td);
+     // console.log(sd);
+     // console.log(ed);
+ 
+     if (td >= sd && td <= ed) {
+       return discount;
+     } else {
+       return [];
+     }
+   }
